e5c120738def40f34ee6c36ca3d874ae61886e14
Edison Gudiño <egudino@montran.com>
2021-05-05 12:08:19 -0500

    Initial Commit

--- PassCodeExample/ContentView.swift
+++ PassCodeExample/ContentView.swift
@@ -6,16 +6,56 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+   @State private var showingAlert = false
+   @State var message = ""
+   var body: some View {
+      
+      VStack{
+         Button(action: {
+            authenticateUser()
+         } , label: {
+            Text("Use Touch ID or Passcode")
+         })
+      }
+      .alert(isPresented: $showingAlert) {
+         Alert(title: Text("Local Auth"), message: Text("\(message)"), dismissButton: .default(Text("Got it!")))
+      }
+      
+   }
+   
+   
+   func authenticateUser() {
+      let authenticationContext = LAContext()
+      var error: NSError?
+      let reasonString = "Unlock App."
+      
+      // Check if the device can evaluate the policy.
+      if authenticationContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthentication, error: &error) {
+         
+         authenticationContext.evaluatePolicy( .deviceOwnerAuthentication, localizedReason: reasonString, reply: { (success, evalPolicyError) in
+            
+            if success {
+               showingAlert = true
+               message = "Success"
+            } else {
+               // Handle evaluation failure or cancel
+               showingAlert = true
+               message = "Incorrect Passcode"
+            }
+         })
+         
+      } else {
+         showingAlert = true
+         message = "Passcode not set"
+      }
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
 }
