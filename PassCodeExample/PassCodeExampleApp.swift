893a809785249a8819432fc4cd70cd579acdb452
Edison Gudiño <egudino@montran.com>
2021-05-05 11:39:53 -0500

    Initial Commit

---
+++ PassCodeExample/PassCodeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassCodeExampleApp.swift
+//  PassCodeExample
+//
+//  Created by Edison Gudiño on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct PassCodeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
